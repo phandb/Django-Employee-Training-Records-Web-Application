4e915e8885cf20bdf210d042219a7b189104849d
phandb72 <phandb72@gmail.com>
2022-06-16 17:00:27 -0500

    Register Functionality

--- main/users/views.py
+++ main/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import RegisterUserForm
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterUserForm()
+        return render(request, 'users/register.html', {'form': form})
+
+
