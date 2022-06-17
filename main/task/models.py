ab96d276bffe29713aaf07eaba0bf0b41e3a6851
phandb72 <phandb72@gmail.com>
2022-06-17 07:59:49 -0500

    Create and Migrate Table Task

--- main/task/models.py
+++ main/task/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    date_taken = models.DateTimeField(default=timezone.now)
+    date_expired = models.DateTimeField(default=timezone.now)
+
+    # Define OneToMany Relationship
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task_name
