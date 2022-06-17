52a0202f71d7672bad0231e1b79354715ee4534f
phandb72 <phandb72@gmail.com>
2022-06-16 21:18:40 -0500

    Login Logout Functionality

--- main/main/settings.py
+++ main/main/settings.py
@@ -138,3 +138,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'  # use Bootstrap 4
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'task-home'
+
+LOGIN_URL = 'task-about'
--- main/main/urls.py
+++ main/main/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register', include('users.urls')),
+    path('register/', include('users.urls')),
+    path('login/', auth.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('task.urls')),
 
 ]
