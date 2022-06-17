3523e5e30f9070f2de76a07be0ed147ed9eb8daa
phandb72 <phandb72@gmail.com>
2022-06-16 21:20:50 -0500

    Add Login Decorator

--- main/task/views.py
+++ main/task/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 # Create your views here.
-
+@login_required()
 def home_page(request):
     return render(request, 'task/home.html')
 
@@ -11,9 +12,11 @@ def about_page(request):
     return render(request, 'task/about.html')
 
 
+@login_required()
 def add_task(request):
     return render(request, 'task/add-task.html')
 
 
+@login_required()
 def edit_task(request):
     return render(request, 'task/edit-task.html')
