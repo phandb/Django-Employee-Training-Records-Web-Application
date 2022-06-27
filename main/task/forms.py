325a47bf690957008af0f0abe87b8c0c75ac3bca
phandb72 <phandb72@gmail.com>
2022-06-26 23:52:37 -0500

    Implement Form Validation for Unique Task and Category

--- main/task/forms.py
+++ main/task/forms.py
@@ -1,6 +1,7 @@
 from bootstrap_datepicker_plus.widgets import DateTimePickerInput
 from django import forms
 
+from . import models
 from .models import Task
 
 METHODS = (
@@ -36,6 +37,12 @@ CATEGORIES = (
 
 
 class TaskForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super(TaskForm, self).__init__(*args, **kwargs)
+        # self.fields["task_name"].queryset = Task.objects.filter(user=self.request.user)
+
     task_name = forms.CharField(
         max_length=100,
         widget=forms.Select(choices=METHODS),
@@ -60,3 +67,47 @@ class TaskForm(forms.ModelForm):
             'date_taken',
         ]
 
+    def clean(self):
+        cleaned_data = super().clean()
+        task_name = cleaned_data.get('task_name')
+        category = cleaned_data.get('category')
+
+        tasks = Task.objects.filter(user=self.request.user)
+
+        if tasks.filter(category=category, task_name=task_name):
+            raise forms.ValidationError('Task name with the category already exists.  Please select different task or '
+                                        'category')
+
+
+class TaskEditForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super(TaskEditForm, self).__init__(*args, **kwargs)
+        # self.fields["task_name"].queryset = Task.objects.filter(user=self.request.user)
+        # self.fields['task_name'].disabled = True
+        # self.fields['category'].disabled = True
+
+    task_name = forms.CharField(
+        max_length=100,
+        widget=forms.Select(choices=METHODS),
+    )
+    category = forms.CharField(
+        max_length=100,
+        widget=forms.Select(choices=CATEGORIES),
+    )
+    date_taken = forms.DateTimeField(
+        input_formats=['%Y/%m/%d %H:%M'],
+        widget=forms.DateTimeInput(attrs={
+            'class': 'form-control datetimepicker-input',
+
+        })
+    )
+
+    class Meta:
+        model = Task
+        fields = [
+            'task_name',
+            'category',
+            'date_taken',
+        ]
--- main/task/views.py
+++ main/task/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Task
-from .forms import TaskForm
+from .forms import TaskForm, TaskEditForm
 
 
 @login_required()
@@ -20,17 +20,18 @@ def about_page(request):
 
 @login_required()
 def add_task(request):
+
     if request.method == 'POST':
-        form = TaskForm(request.POST)
+        form = TaskForm(request.POST, request=request)
         form.instance.user = request.user
 
         if form.is_valid():
+
             form.save()
             return redirect('task-home')
     else:
-        form = TaskForm()
+        form = TaskForm(request=request)
 
-    # tasks = Task.objects.filter(user=request.user.id)
     context = {
         # 'tasks': tasks.order_by('task_name'),
         'form': form
@@ -43,12 +44,12 @@ def edit_task(request, task_id):
     data = Task.objects.get(id=task_id)
 
     # update_screen = True if update == 'update' else False
-    form = TaskForm(request.POST or None)
+    form = TaskEditForm(request.POST or None, request=request)
     if request.method == 'POST':
 
         if form.is_valid():
-            data.task_name = form.instance.task_name
-            data.category = form.instance.category
+            data.task_name = data.task_name
+            data.category = data.category
             data.date_taken = form.instance.date_taken
             data.save()
             return redirect('task-home')
@@ -58,7 +59,7 @@ def edit_task(request, task_id):
             'category': data.category,
             'date_taken': data.date_taken
         }
-        form = TaskForm(initial=form_data)
+        form = TaskEditForm(initial=form_data, request=request)
 
     context = {
         'form': form
